a0d890826b7a9e6fd980a9116a30a2ee40f0a174
lucas <wymam1@gmail.com>
2024-12-29 14:59:23 +0000

    modify get_klines

--- preprocessing/get_klines.py
+++ preprocessing/get_klines.py
@@ -24,19 +24,29 @@ def get_klines(symbol, interval, start_time=None, end_time=None, limit=None):
         value = res.json()
 
         tmp = pd.DataFrame(value, columns=columns)
+        
         latest = int(tmp.iat[-1,0])
         if start_time == latest:
             break
         start_time = latest
         df = pd.concat([df,tmp])
+        time.sleep(0.075)
+
+    df['Open time'] = df['Open time'].astype('int')
+    df['Open time'] = df['Open time'].apply(lambda x : datetime.fromtimestamp(x/1000))
+    df['Close time'] = df['Close time'].astype('int')
+    df['Close time'] = df['Close time'].apply(lambda x : datetime.fromtimestamp(x/1000))
+    df = df.set_index('Open time')
 
     return df
 
 #timestamp = 1685577600000
 #23년 6월 1일 오전 9시의 타임스탬프
 #timestamp = 1732792920000
-date_string = '2024-12-28 18:00:00'
-timestamp = int(time.mktime(datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S').timetuple())*1000)
-current = math.trunc(int((time.time()/100))*100000)
+if __name__ == '__main__':
+    date_string = '2024-12-28 18:00:00'
+    timestamp = int(time.mktime(datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S').timetuple())*1000)
+    current = math.trunc(int((time.time()/100))*100000)
 
-print(get_klines("BTCUSDT", "1h", start_time=timestamp, end_time=current))
+    df =  get_klines("BTCUSDT", "1h", start_time=timestamp, end_time=current)
+    df.to_csv("./BTCUSDT.csv")
