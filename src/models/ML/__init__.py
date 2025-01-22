fba9b81da79bbc25a248364863391723d86f6a1d
lucas <wymam1@gmail.com>
2025-01-22 07:09:29 +0000

    combine predict, train function

--- src/models/ML/__init__.py
+++ src/models/ML/__init__.py
@@ -1,5 +1,4 @@
 from .LSTM import LSTM
-from .train import train
-from .predict import predict
+from .utils import train, test, predict
 from .dataset.LSTMDataset import LSTMDataset
 from .targets import create_targets
--- src/models/ML/utils.py
+++ src/models/ML/utils.py
@@ -2,7 +2,7 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 from sklearn.metrics import accuracy_score
-
+import pandas as pd
 # 모델 불러오기(존재하면 추가 학습, 없다면 새로 학습)
 # 데이터 불러오기
 # 학습하기
@@ -73,3 +73,66 @@ def train(model, train_loader, val_loader, epochs, learning_rate, device):
               f"Val Loss: {val_loss/len(val_loader):.4f}, Val Acc: {val_accuracy:.4f}")
 
     print("Training Complete") 
+
+
+def test(model, pred_loader, device):
+    """
+    실시간 예측 함수
+    :param model: 학습된 LSTM 모델
+    :param input_data: 새로운 입력 데이터 (시퀀스 형태)
+    :param device: 사용할 디바이스 (CPU/GPU)
+    :return: 매수(0), 매도(1), 대기(2) 중 하나
+    
+    model.eval()
+    with torch.no_grad():
+        input_tensor = torch.tensor(input_data, dtype=torch.float32).unsqueeze(0).to(device)
+        print(input_tensor.shape)
+        outputs = model(input_tensor)
+        _, predicted = torch.max(outputs, 1)
+        return predicted.item()
+    """
+    model.eval()
+
+    pred_targets = []
+    pred_predictions = []
+    with torch.no_grad():
+        for features, targets in pred_loader:
+            features, targets = features.to(device), targets.to(device)
+            outputs = model(features)
+
+            _, predicted = torch.max(outputs, 1)
+            pred_predictions.extend(predicted.cpu().numpy())
+            pred_targets.extend(targets.cpu().numpy())
+    pred_accuracy = accuracy_score(pred_targets, pred_predictions)
+    print(f"Predict Acc: {pred_accuracy:.4f}")
+    
+
+# 입력 데이터에 대한 JSON 형태로 출력을 반환
+
+def predict(model, input_data, device):
+    """
+    실시간 예측 함수
+    :param model: 학습된 LSTM 모델
+    :param input_data: 새로운 입력 데이터 (시퀀스 형태)
+    :param device: 사용할 디바이스 (CPU/GPU)
+    :return: 매수(0), 매도(1), 대기(2) 중 하나
+    
+    model.eval()
+    with torch.no_grad():
+        input_tensor = torch.tensor(input_data, dtype=torch.float32).unsqueeze(0).to(device)
+        print(input_tensor.shape)
+        outputs = model(input_tensor)
+        _, predicted = torch.max(outputs, 1)
+        return predicted.item()
+    """
+    model.eval()
+ 
+    with torch.no_grad():
+        input_tensor = torch.tensor(input_data.to_numpy(), dtype=torch.float32).unsqueeze(0).to(device)
+        outputs = model(input_tensor)
+
+        _, predicted = torch.max(outputs, 1)
+    
+    
+    print("실시간 예측 결과:", ["매수", "매도", "대기"][predicted.item()])
+    #print("실제 결과 : ", new_data['Target'])
