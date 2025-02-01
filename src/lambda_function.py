7763325ff5a4d3264d6521e1624fe0f4430e6fa7
lucas <wymam1@gmail.com>
2025-02-01 07:13:30 +0000

    create lambda function

---
+++ src/lambda_function.py
@@ -0,0 +1,31 @@
+import torch
+
+from models.ML import LSTM, predict, create_targets
+from data import getCurrentData
+import json
+
+
+
+# Model의 하이퍼파라미터 및 데이터 설정
+input_size = 6  # 예: OHLCV + RSI + MACD + EMA
+hidden_size = 64
+num_layers = 2
+output_size = 3  # 매수, 매도, 대기
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+
+
+def lambda_handler(event, context):
+    model = LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, output_size=output_size)
+    model.load_state_dict(torch.load('LSTM-20250122.pt'))
+
+    new_data = getCurrentData("BTCUSDT", "1h", limit=12)
+
+
+    side = predict(model, new_data, device)
+    
+    return {
+        "side":side
+    }
